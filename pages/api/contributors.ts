9646eb4bd65c82e2ce9333f5d761d1b6d176b2dd
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-08 21:03:21 +0100

    Update contributors.ts

--- pages/api/contributors.ts
+++ pages/api/contributors.ts
@@ -35,7 +35,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       const contributors = await fetchGitHubApi(`api.github.com{ORG_NAME}/${repo}/contributors?per_page=100`);
       
       for (const contributor of contributors) {
-        const { login, contributions, avatar_url, html_url } = contributor;
+        const { logi, contributions, avatar_url, html_url } = contributor;
         if (login in allContributors) {
           allContributors[login].commits += contributions;
         } else {

69154016c9ea7a7154aaa96686ab2eb6d8e80e16
Investorphem <39197206+investorphem@users.noreply.github.com>
2025-12-10 19:26:20 +0100

    Update contributors.ts

--- pages/api/contributors.ts
+++ pages/api/contributors.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-// Customize this list for specific repos you wa
+// Customize this list for specific repos you w
 const ORG_NAME = 'stacks-network'; 
 const REPO_NAMES = ['stacks-core', 'stacks-blockchain-api', 'stacks-blockchain-docker', 'stacking-explorer']; 
 
